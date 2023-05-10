63dc9b520c7070549436ed0fb6ebb4bf7ffbcf28
Maxime Lelong <maximelelong3@gmail.com>
2023-05-10 14:27:11 +0200

    Passage sous CMake + update Readme

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # bomberman
 Jeu bomberman en C++
+
+## CMake build
+Pour compiler le projet, exécuter les commandes suivantes à la racine du projet:
+```bash
+mkdir build  # Création du dossier build
+cd build
+cmake .. # Configuration du projet
+cmake --build . # Compilation du projet
+```
---
+++ build/src/CMakeFiles/bomberman.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/maxime/Documents/Polytech Sorbonne/ROB4/POO/Projet C++/bomberman/src/Case.cpp" "src/CMakeFiles/bomberman.dir/Case.cpp.o" "gcc" "src/CMakeFiles/bomberman.dir/Case.cpp.o.d"
+  "/home/maxime/Documents/Polytech Sorbonne/ROB4/POO/Projet C++/bomberman/src/Game.cpp" "src/CMakeFiles/bomberman.dir/Game.cpp.o" "gcc" "src/CMakeFiles/bomberman.dir/Game.cpp.o.d"
+  "/home/maxime/Documents/Polytech Sorbonne/ROB4/POO/Projet C++/bomberman/src/GameElement.cpp" "src/CMakeFiles/bomberman.dir/GameElement.cpp.o" "gcc" "src/CMakeFiles/bomberman.dir/GameElement.cpp.o.d"
+  "/home/maxime/Documents/Polytech Sorbonne/ROB4/POO/Projet C++/bomberman/src/Player.cpp" "src/CMakeFiles/bomberman.dir/Player.cpp.o" "gcc" "src/CMakeFiles/bomberman.dir/Player.cpp.o.d"
+  "/home/maxime/Documents/Polytech Sorbonne/ROB4/POO/Projet C++/bomberman/src/Terrain.cpp" "src/CMakeFiles/bomberman.dir/Terrain.cpp.o" "gcc" "src/CMakeFiles/bomberman.dir/Terrain.cpp.o.d"
+  "/home/maxime/Documents/Polytech Sorbonne/ROB4/POO/Projet C++/bomberman/src/main.cpp" "src/CMakeFiles/bomberman.dir/main.cpp.o" "gcc" "src/CMakeFiles/bomberman.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
